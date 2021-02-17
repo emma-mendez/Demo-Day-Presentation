3cc0dc0c7770a97349b8f9327173d20ffa7841a0
Emma Mendez <emmamendez07@gmail.com>
2021-02-17 23:07:33 +0000

    small changes

---
+++ components/Homepage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Dashboard from '..components/';
+
+export default class Homepage extends Component {
+    render() {
+        return (
+            <div>
+                <Dashboard />
+            </div>
+        )
+    }
+}
